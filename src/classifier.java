c938a8e9c48ff653f509b1dc111e6f09335e4fb2
Martin Boldt <martin.boldt@bth.se>
2012-01-26 16:33:00 +0100

    Added skelleton

---
+++ src/classifier.java
@@ -0,0 +1,19 @@
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+
+public class classifier {
+	private Classifier classifier;
+	
+	public classifier( Instances instances ) {
+		
+	}
+	
+	public int start() {
+		return 1;
+	}
+	
+	public String getVerdict() {
+		return "";
+	}
+	
+}
