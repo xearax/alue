342ffbb21dffceccd9f1dccc62b2079bda34f5d4
Martin Boldt <martin.boldt@bth.se>
2012-01-28 15:12:30 +0100

    Fixed the traverse function and related stuff

--- src/logic.java
+++ src/logic.java
@@ -1,36 +1,59 @@
+import java.io.*;
+import java.util.Vector;
+
 public class logic {
-	private String verdict[];
-	private String path;
+	private Vector<String> verdict;
+	private File path;
 	
-	public logic( String path ) {
-		this.path = path;
+	public logic( String inPath ) {
+		path = new File( inPath );
+		verdict = new Vector<String>();
 	}
 
-	public int start() {
-		extractor extract = new extractor( path );
-		if ( extract.start() != 0 ) {
-			misc.log( "Error: <error-message>" );
-			return 1;
-		}
-		preprocess pprocess = new preprocess( extract.getLicense() );
-		pprocess.start();
+	public int start() {		
 		
-		classifier cfier = new classifier( pprocess.getInstances() );
-		cfier.start();
-		//cfier.getVerdict();
+		traverse( path );
 		
 		return 0;
 	}
 	
-	private boolean isFileOK() {
-		return false;
-	}
-	
-	private void traverse( String path ) {
+	private boolean isFileOK( File inPath) {
 		
+		if ( inPath.canRead() ) {
+			// Also check if file has supported file suffix, e.g. .exe, .msi ...
+			return true;
+		}
+		else {
+			misc.log( "Info: " + inPath.getAbsolutePath() + "is not readable." );
+			return false;
+		}
 	}
 	
-	public String getVerdict() {
-		return "";
+	private void traverse( File inPath ) {
+		if ( inPath.isDirectory() ) {
+			File fileList[] = inPath.listFiles();
+			
+			for (int count=0; count < fileList.length; count++ ) {
+				traverse( fileList[ count ] );
+			}
+		} else if ( inPath.isFile() ) {
+			if ( isFileOK( inPath ) ) {				
+				extractor Xtractor = new extractor( inPath.getAbsolutePath() );
+				preprocess pprocess = new preprocess( Xtractor.getLicense() );
+				pprocess.start();
+				classifier cfier = new classifier( pprocess.getInstances() );
+				cfier.start();
+				
+				//Temporarily just adding path to verdict vector
+				verdict.add( inPath.getAbsolutePath() );
+				//verdict.add( cfier.getVerdict() );
+			}
+		} else {
+			misc.log( "Info: " + inPath.getAbsolutePath() + "is neither file nor directory." );
+		}
+	}
+	
+	public Vector<String> getVerdict() {
+		return verdict;
 	}
 }
