86ee6ac0e5c4056815bccdd984e6fc4591b0cd8d
Martin Boldt <martin.boldt@bth.se>
2012-01-31 16:44:15 +0100

    New class to hold classification verdict results

---
+++ src/verdict.java
@@ -0,0 +1,17 @@
+public class verdict {
+	private String name;
+	private String result;
+	
+	public verdict( String inName, String inResult ) {
+		name = inName;
+		result = inResult;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getResult() {
+		return result;
+	}
+}
